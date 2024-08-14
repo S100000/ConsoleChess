ff9e1db8d7598f2c856db0b6c4c07aefb6e6446a
José Luiz <redg7338@gmail.com>
2024-08-14 13:52:31 -0300

    Piece Constructor

--- ConsoleChess/board/Piece.cs
+++ ConsoleChess/board/Piece.cs
@@ -6,5 +6,13 @@
         public Color Color { get; set; }
         public int qtdMovements { get; protected set; }
         private Board board { get; set; }
+
+        public Piece (Color color, Board board)
+        {
+            Position = null;
+            Color = color;
+            this.qtdMovements = 0;
+            this.board = board;
+        }
     }
 }
